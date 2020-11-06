4e3de7f857d3f87fdd8864a16e84ec67815e7f8b
63070109 <73678996+63070109@users.noreply.github.com>
2020-11-06 17:02:43 +0700

    Add files via upload

--- div.php
+++ div.php
@@ -1,44 +1,30 @@
-<!DOCTYPE html>
+<!DOCTYPE html> 
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Hello</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" 
+    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" 
+    crossorigin="anonymous">>
 </head>
 <body>
     <div class="container">
-        <div class = "row">
-            <div class= "col-sm-6 col-lg-6 col-xl-6 col-6 col-md-6">
-                <img src="pic109/t2.jpg" class ="img-fluid rounded" style="height: 50%;">
+        <div class="row">
+            <div class="col-sm-6">
+                <img src="pic109/t2.jpg" alt="" width="425" height="600">
             </div>
-            <div class= "col-sm-3 col-lg-3 col-xl-3 col-3 col-md-3">
-                <div class="row">
-                    <div class="col">
-                        <img src="pic109/t1.jpg" class ="img-fluid rounded">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <img src="pic109/t4.jpg" class ="img-fluid rounded">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <img src="pic109/t3.jpg" class ="img-fluid rounded">
-                    </div>
-                </div>
+            <div class="col-sm-3">
+                <img src="pic109/t1.jpg" alt="" width="250" height="200">
+                <img src="pic109/t4.jpg" alt="" width="250" height="200">
+                <img src="pic109/t3.jpg" alt="" width="250" height="200">
             </div>
-            <div class= "col-sm-3 col-lg-3 col-xl-3 col-3 col-md-3">
-                    <div class="row">
-                        <img src="pic109/t5.jpg" class ="img-fluid rounded" style="height: 250px;">
-                    </div>
-                    <div>
-                        <img src="pic109/t6.jpg" class ="img-fluid rounded" style="height: 250px;">
-                    </div>
+            <div class="col-sm-3">
+                <img src="pic109/t5.jpg" alt="" width="500" height="400"><br>
+                <img src="pic109/t6.jpg" alt="" width="500" height="200">
             </div>
         </div>
-    </div>                
+    </div>
 </body>
 </html>
